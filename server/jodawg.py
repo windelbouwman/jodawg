a9efc026e4b45f0619eb2f3cc659ef00d2822469
Windel Bouwman <windel@windel.nl>
2013-07-31 21:12:34 +0200

    Expanded example

--- server/jodawg.py
+++ server/jodawg.py
@@ -1,4 +1,17 @@
-import tornado
+import tornado.ioloop
+import tornado.options
+import tornado.web
 
-print 'hello world'
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
 
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([(r"/", MainHandler)])
+    application.listen(tornado.options.options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
+    
