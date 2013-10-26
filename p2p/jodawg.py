20baf6ed3bf520aaacd3af0eb8a350ec65b4033b
almer <almer@gnome.org>
2013-10-26 23:05:14 +0200

    * jodawg.py: Updated with some thoughts / changes on the wire protocol.

--- p2p/jodawg.py
+++ p2p/jodawg.py
@@ -328,8 +328,21 @@ class User(object):
         self.identifier = 1
         self.nickname = "default"
 
-    def to_xml(self):
-        return "<"
+
+class Node(object):
+    """A Node represents a peer in the network. A peer
+       always has a (socket) address and a public_key
+       for communication. A node may or may not have
+       a user associated with it. Though, each node can
+       have AT MOST one user.
+    """
+
+    __slots__ = [ "address", "public_key", "none" ]
+
+    def __init__(self):
+        self.address = None
+        self.public_key = None
+        self.user = None
 
 class Node(object):
     __slots__ = [ "context", "super_peers_locations" ]
@@ -370,3 +383,39 @@ class Node(object):
 #node = Node()
 #node.run()
 
+# Thoughts on on-the-wire protocol:
+# * protobuf has the disadvantage of not including the protocol
+#   specification in the wire format (hence, it requires sender/receiver
+#   to HAVE the full spec - and the same version of it as well). Though,
+#   it is the most compact option. The default Python implementation is
+#   slow.
+# * XML is perhaps the most widely used solution (think XMPP), but is
+#   slow and verbose. Though, it can be compressed, XML parsers have
+#   numerous disadvantages (including security problems).
+# * JSON is basically a lightweight XML-like format, though not
+#   hampered by its verbosity, nor vulnerable to the same security
+#   issues. Though more verbose than protobuf, it can be compressed
+#   down to a reasonable size easily (either using zlib, or python-blosc
+#   as much faster solution). ZMQ also offers easy object serialization
+#   using JSON as carry format. So, it seems a good choice for now.
+#
+# See http://zeromq.github.io/pyzmq/serialization.html for some
+# more thoughts on this.
+
+# PROTOCOL MESSAGES
+
+# Session Control:
+#
+# request: register_user <user-identifier> <public_key>
+# request: login <user-identifier>
+# request: logout <user-identifier>
+# request: set_status <user-identifier> <status>
+# request: set_profile <user-identifier> <vcard_data>
+
+# Messaging:
+# 
+# request: message-log-join <user-identifier>
+# request: message-log-leave <user-identifier>
+# request: message-log-synchronize <user-identifier>
+# request: message-log-append <user-identifier>
+#
